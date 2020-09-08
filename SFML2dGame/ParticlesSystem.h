5c4344cab52fbeb7742b7939bb5668e1341b4be1
makarenk0 <45842815+makarenk0@users.noreply.github.com>
2020-09-08 14:00:48 +0300

    description images

--- SFML2dGame/ParticlesSystem.h
+++ SFML2dGame/ParticlesSystem.h
@@ -15,14 +15,10 @@ using std::byte;
 class ParticlesSystem
 {
 private:
-	float modOfVector(sf::Vector2f& v);
 	void particleTransform(std::vector<Particle>const& part, float**& i_vec, int numb_of_block, int part_in_block);
 	std::vector<ParticlesSet*> _all_sets;
 
 public:
-
-	
-	//void testParticles(sf::RenderWindow& window); //sf::RenderWindow& window
 
 	void addParticlesEffect(ParticlesSet* p_set);
 	void update(sf::RenderWindow& window, float dt);
